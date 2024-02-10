66a29e3d661660a0da987ffb808017ba236b96e6
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-02-10 16:24:00 +0530

    added 9 squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Square from "./Components/Square/Square";
+
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
 const App = () => {
   return (
-    <div className='main-div'>
-      <div className='container'>
-        <h1 className='game-heading bg-style'>Tic Tac Toe</h1>
+    <div className="main-div">
+      <div>
+        <div className="move-detection">
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+        <h1 className="game-heading bg-style">Tic Tac Toe</h1>
+
+        <div className="square-wrapper">
+          {renderFrom.map((arr) =>
+            arr.map((e) => {
+              return <Square />;
+            })
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
