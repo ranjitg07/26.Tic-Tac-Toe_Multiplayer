cd79de0573efc3004eb72c1e51fba9ae55e92d1c
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-02-13 15:27:27 +0530

    store user in array, set online true & false when user disconnected

--- Server/server.js
+++ Server/server.js
@@ -5,9 +5,21 @@ const io = new Server(httpServer, {
   cors: "http://localhost:5173/",
 });
 
+const allUsers = [];
+
 io.on("connection", (socket) => {
-//   console.log(socket);
-console.log("New user joined webaocket" + socket.id);
+  allUsers.push({
+    socket: socket,
+    online: true,
+  });
+  socket.on("disconnect", function () {
+    for (let index = 0; index < allUsers.length; index++) {
+      const user = allUsers[index];
+      if (user.id === socket.id) {
+        user.online = false;
+      }
+    }
+  });
 });
 
 httpServer.listen(3000);
